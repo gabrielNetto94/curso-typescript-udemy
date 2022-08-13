8d311b23cf67c9b5446dd1aeb08a13c107202e14
gabrielNetto94 <silveiranetto94@gmail.com>
2022-08-13 15:33:40 -0300

    Continuação do curso

--- index.js
+++ index.js
@@ -1,36 +1,74 @@
 "use strict";
-console.log(1);
 let firstName = '2';
 let flag = false;
 let number = 3;
-function printName(name) {
-    console.log('olá' + name);
-}
 function sumNumbers(number1, number2) {
     return number1 + number2;
 }
 //inference and annotation
-let annotation = 'teste';
-console.log(annotation);
-//let inference = 'as'
+let annotation = 'teste'; //tipa o dado
+let inference = 'as'; //deixa o typescript tipar
 function convertNumberToString(number) {
     return `Número convetido para string ${number.toString()}`;
 }
-console.log(convertNumberToString(9));
-let strings;
-let numbers = [];
-let arrayWhatever = [];
-arrayWhatever.push(2);
-arrayWhatever.push('231');
-arrayWhatever.push({ chave: 'valor' });
+//console.log(convertNumberToString(9))
+let strings; //apenas string
+let numbers = []; //apenas números
+let arrayAny = []; //qualquer dado
+arrayAny.push(2);
+arrayAny.push('231');
+arrayAny.push({ chave: 'valor' });
 numbers.push(90);
 strings = ['gabriel', 'netto', 'blabla'];
-console.log(arrayWhatever);
-console.log(numbers);
-console.log(strings);
-function print(texto) {
-    return `Seu texto é: ${texto}`;
+//console.log(arrayAny)
+//console.log(numbers)
+//console.log(strings)
+//tipa o parâmetro e retorno
+function printText(texto) {
+    return `Your text is ${texto}`;
 }
-function example(person) {
-    return { name: 'asdasd' };
+//tipa o objeto
+function printPerson(person) {
+    return `Hello ${person.name}, your age is ${person.age}`;
 }
+//console.log(printPerson({ name: 'Gabriel', age: 28 }))
+function showNumber(number1, number2) {
+    return number2 !== undefined ? number1 * number2 : number1 * 2;
+}
+//console.log(showNumber(2, 10));
+//Union Type, permite parâmetro de diferentes tipos
+function unionType(value) {
+    return `Saldo da conta é ${value}`;
+}
+//console.log(unionType([1, '1']));
+//
+function showBalance(value) {
+    return `Saldo da conta é ${value}`;
+}
+//console.log(showBalance("23"));
+function showUserRole(role) {
+    if (typeof role === 'boolean')
+        return 'Usuário não autenticado';
+    return `User ${role}`;
+}
+function typeAlias(id, person) {
+    return `ID: ${id}, Name = ${person.name} - Age = ${person.age}`;
+}
+const coordObj = {
+    x: 1.123,
+    y: 14.1233,
+    z: 9.9932,
+};
+function showCoord(obj) {
+    console.log(`X = ${obj.x} - Y = ${obj.y} - Z = ${obj.z} `);
+}
+// Literal types
+function showDirection(direction) {
+    return direction;
+}
+//console.log(showDirection('right'))
+// Non-null Assertion operators
+// ! informa para o TS que o elemento não é nulo
+const p = document.getElementById('some-p');
+console.log(p.innerText);
+// Bigint
--- index.ts
+++ index.ts
@@ -1,46 +1,122 @@
-console.log(1)
 let firstName = '2'
 let flag = false
 let number = 3
-
-function printName(name: string) {
-    console.log('olá' + name)
-}
 
 function sumNumbers(number1: number, number2: number) {
     return number1 + number2
 }
 
 //inference and annotation
-let annotation: string = 'teste'
-console.log(annotation)
-//let inference = 'as'
+let annotation: string = 'teste' //tipa o dado
+let inference = 'as' //deixa o typescript tipar
 
 function convertNumberToString(number: number) {
     return `Número convetido para string ${number.toString()}`
 }
+//console.log(convertNumberToString(9))
 
-console.log(convertNumberToString(9))
+let strings: Array<string> //apenas string
+let numbers: number[] = []; //apenas números
+let arrayAny: any = [] //qualquer dado
 
-let strings: Array<string>
-let numbers: number[] = [];
-let arrayWhatever: any = []
-
-arrayWhatever.push(2)
-arrayWhatever.push('231')
-arrayWhatever.push({ chave: 'valor' })
+arrayAny.push(2)
+arrayAny.push('231')
+arrayAny.push({ chave: 'valor' })
 numbers.push(90)
 strings = ['gabriel', 'netto', 'blabla']
 
-console.log(arrayWhatever)
-console.log(numbers)
-console.log(strings);
+//console.log(arrayAny)
+//console.log(numbers)
+//console.log(strings)
 
-function print(texto: string): string {
-    return `Seu texto é: ${texto}`
+//tipa o parâmetro e retorno
+function printText(texto: string): string {
+    return `Your text is ${texto}`
 }
 
-function example(person: { name: string, age: number }) {
-
-    return { name: 'asdasd' }
+//tipa o objeto
+function printPerson(person: { name: string, age: number }) {
+    return `Hello ${person.name}, your age is ${person.age}`
 }
+//console.log(printPerson({ name: 'Gabriel', age: 28 }))
+
+function showNumber(number1: number, number2?: number): number {
+
+    return number2 !== undefined ? number1 * number2 : number1 * 2
+}
+//console.log(showNumber(2, 10));
+
+//Union Type, permite parâmetro de diferentes tipos
+function unionType(value: Array<number | string>) {
+
+    return `Saldo da conta é ${value}`
+}
+//console.log(unionType([1, '1']));
+
+//
+function showBalance(value: number | string): number | string {
+
+    return `Saldo da conta é ${value}`
+}
+//console.log(showBalance("23"));
+
+
+function showUserRole(role: boolean | string) {
+    if (typeof role === 'boolean')
+        return 'Usuário não autenticado'
+
+    return `User ${role}`
+}
+//console.log(showUserRole(false))
+//console.log(showUserRole('Admin'))
+
+//Type alias
+type ID = string | number
+
+type PersonTeste = {
+    name: string,
+    age: number
+}
+function typeAlias(id: ID, person: PersonTeste) {
+    return `ID: ${id}, Name = ${person.name} - Age = ${person.age}`
+}
+//console.log(typeAlias(1, { name: 'Gabriel', age: 28 }))
+
+interface Point {
+    x: number
+    y: number
+    z: number
+}
+
+const coordObj: Point = {
+    x: 1.123,
+    y: 14.1233,
+    z: 9.9932,
+}
+function showCoord(obj: Point) {
+    console.log(`X = ${obj.x} - Y = ${obj.y} - Z = ${obj.z} `)
+}
+//showCoord(coordObj)
+
+//Interface x Type alias
+interface Person {
+    name: string
+}
+
+interface Person {
+    age: string
+}
+
+// Literal types
+function showDirection(direction: 'left' | 'top' | 'right' | 'bottom') {
+    return direction
+}
+//console.log(showDirection('right'))
+
+// Non-null Assertion operators
+// ! informa para o TS que o elemento não é nulo
+const p = document.getElementById('some-p')
+console.log(p!.innerText)
+
+// Bigint
+

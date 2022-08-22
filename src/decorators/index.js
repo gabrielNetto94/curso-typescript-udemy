cadab6c05d156b2fd0b13ce01db29b0db1717525
gabrielNetto94 <silveiranetto94@gmail.com>
2022-08-22 19:19:37 -0300

    Finalização seção decorators

--- src/decorators/index.js
+++ src/decorators/index.js
@@ -23,8 +23,9 @@ class Myclass {
 __decorate([
     myDecorator()
 ], Myclass.prototype, "testing", null);
-const myObj = new Myclass();
+//const myObj = new Myclass()
 //myObj.testing()
+//------------------------------------------------------------------------------------
 //decorators multiplos
 function decoratorA() {
     return function (target, property, descriptor) {
@@ -52,8 +53,9 @@ __decorate([
     decoratorB(),
     decoratorA()
 ], MultipleDecorators.prototype, "testingMultipleDecorators", null);
-const mutiple = new MultipleDecorators();
+//const mutiple = new MultipleDecorators()
 //mutiple.testingMultipleDecorators()
+//------------------------------------------------------------------------------------
 //class decorator
 function classDecorator(constructor) {
     console.log(constructor);
@@ -69,10 +71,11 @@ let User = class User {
 User = __decorate([
     classDecorator
 ], User);
-const paulo = new User('Paulo bla');
+//const paulo = new User('Paulo bla')
 //Method decorator
 function enumerable(value) {
     return function (target, property, descriptor) {
+        descriptor.enumerable = value;
     };
 }
 class Machine {
@@ -80,7 +83,152 @@ class Machine {
         this.name = name;
     }
     showName() {
+        console.log(this);
         return 'Nome da máquina é: ' + this.name;
     }
 }
+__decorate([
+    enumerable(false)
+], Machine.prototype, "showName", null);
 const trator = new Machine('Tratorzão brabo');
+//console.log(trator.showName())
+//------------------------------------------------------------------------------------
+//Accessor decorator
+class Monster {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    get showName() {
+        return `Nome do monstro ${this.name}`;
+    }
+    get showAge() {
+        return `Idade do monstro ${this.age}`;
+    }
+}
+__decorate([
+    enumerable(true)
+], Monster.prototype, "showName", null);
+const charmander = new Monster('Charmander', 30);
+//console.log(charmander)
+//------------------------------------------------------------------------------------
+//Property decorator
+function formatNumber() {
+    return function (target, propertyKey) {
+        let value;
+        const getter = function () {
+            return value;
+        };
+        const setter = function (newValue) {
+            value = newValue.padStart(5, '0');
+        };
+        Object.defineProperty(target, propertyKey, {
+            set: setter,
+            get: getter
+        });
+    };
+}
+class ID {
+    constructor(id) {
+        this.id = id;
+    }
+}
+__decorate([
+    formatNumber()
+], ID.prototype, "id", void 0);
+const newItem = new ID('1');
+//console.log(newItem)
+////------------------------------------------------------------------------------------
+//Exemplo Class decorator
+function createdDate(created) {
+    created.prototype.createdAt = new Date();
+}
+let Book = class Book {
+    constructor(id) {
+        this.id = id;
+    }
+};
+Book = __decorate([
+    createdDate
+], Book);
+let Pen = class Pen {
+    constructor(id) {
+        this.id = id;
+    }
+};
+Pen = __decorate([
+    createdDate
+], Pen);
+const newBook = new Book(10);
+const newPen = new Pen(12);
+console.log(newBook);
+console.log(newPen);
+//------------------------------------------------------------------------------------
+//Exemplo method decorator
+function checkIfUserPost() {
+    return function (target, key, descriptor) {
+        const childFunction = descriptor.value;
+        //console.log(childFunction)
+        //arumentos da função
+        descriptor.value = function (...args) {
+            if (args[1] === true) {
+                console.log('Usuário já postou');
+                return null;
+            }
+            else {
+                //retorna para continuar a execução da função
+                return childFunction.apply(this, args);
+            }
+        };
+        return descriptor;
+    };
+}
+class Post {
+    constructor() {
+        this.alreadyPosted = false;
+    }
+    post(content, alreadyPosted) {
+        this.alreadyPosted = true;
+        console.log(`Post do usuário: ${content}`);
+    }
+}
+__decorate([
+    checkIfUserPost()
+], Post.prototype, "post", null);
+const newPost = new Post();
+newPost.post('Primeiro Post', newPost.alreadyPosted);
+newPost.post('Segundo Post', newPost.alreadyPosted);
+//------------------------------------------------------------------------------------
+//Property decorator
+function Max(limit) {
+    return function (target, propertyKey) {
+        let value;
+        const getter = function () {
+            return value;
+        };
+        const setter = function (newVal) {
+            if (newVal.length > limit) {
+                console.log(`Valor deve ter no máxmimo ${limit} digitos`);
+            }
+            else {
+                value = newVal;
+            }
+        };
+        Object.defineProperty(target, propertyKey, {
+            get: getter,
+            set: setter
+        });
+    };
+}
+class Admin {
+    constructor(username) {
+        this.username = username;
+    }
+}
+__decorate([
+    Max(10)
+], Admin.prototype, "username", void 0);
+const Admin1 = new Admin('123456789012');
+const Admin2 = new Admin('netto');
+console.log(Admin1);
+console.log(Admin2);
--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -25,15 +25,12 @@ class Myclass {
     }
 }
 
-const myObj = new Myclass()
+//const myObj = new Myclass()
 
 //myObj.testing()
 
-
+//------------------------------------------------------------------------------------
 //decorators multiplos
-
-
-
 function decoratorA() {
 
     return function (
@@ -77,10 +74,10 @@ class MultipleDecorators {
     }
 }
 
-const mutiple = new MultipleDecorators()
-
+//const mutiple = new MultipleDecorators()
 //mutiple.testingMultipleDecorators()
 
+//------------------------------------------------------------------------------------
 //class decorator
 function classDecorator(constructor: Function) {
     console.log(constructor)
@@ -98,7 +95,7 @@ class User {
         this.name = name
     }
 }
-const paulo = new User('Paulo bla')
+//const paulo = new User('Paulo bla')
 
 //Method decorator
 function enumerable(value: boolean) {
@@ -107,6 +104,7 @@ function enumerable(value: boolean) {
         property: string,
         descriptor: PropertyDescriptor
     ) {
+        descriptor.enumerable = value
 
     }
 }
@@ -117,8 +115,188 @@ class Machine {
         this.name = name
     }
 
+    @enumerable(false)
     showName() {
+        console.log(this)
         return 'Nome da máquina é: ' + this.name
     }
 }
 const trator = new Machine('Tratorzão brabo')
+
+//console.log(trator.showName())
+
+//------------------------------------------------------------------------------------
+//Accessor decorator
+
+class Monster {
+    name?
+    age?
+
+    constructor(name: string, age: number) {
+        this.name = name
+        this.age = age
+    }
+
+    @enumerable(true)
+    get showName() {
+        return `Nome do monstro ${this.name}`
+    }
+
+    get showAge() {
+        return `Idade do monstro ${this.age}`
+    }
+}
+
+const charmander = new Monster('Charmander', 30)
+
+//console.log(charmander)
+
+//------------------------------------------------------------------------------------
+//Property decorator
+
+function formatNumber() {
+    return function (target: Object, propertyKey: string) {
+        let value: string
+
+        const getter = function () {
+            return value
+        }
+
+        const setter = function (newValue: string) {
+            value = newValue.padStart(5, '0')
+        }
+
+        Object.defineProperty(target, propertyKey, {
+            set: setter,
+            get: getter
+        })
+    }
+}
+
+class ID {
+    @formatNumber()
+    id
+
+    constructor(id: string) {
+        this.id = id
+    }
+}
+
+const newItem = new ID('1')
+//console.log(newItem)
+
+////------------------------------------------------------------------------------------
+//Exemplo Class decorator
+function createdDate(created: Function) {
+    created.prototype.createdAt = new Date()
+}
+
+@createdDate
+class Book {
+    id
+    createdAt?: Date
+
+    constructor(id: number) {
+        this.id = id
+    }
+}
+
+@createdDate
+class Pen {
+    id
+
+    constructor(id: number) {
+        this.id = id
+    }
+}
+
+const newBook = new Book(10)
+const newPen = new Pen(12)
+
+console.log(newBook)
+console.log(newPen)
+
+//------------------------------------------------------------------------------------
+//Exemplo method decorator
+function checkIfUserPost() {
+
+    return function (
+        target: Object,
+        key: string | Symbol,
+        descriptor: PropertyDescriptor
+    ) {
+        const childFunction = descriptor.value
+        //console.log(childFunction)
+
+        //arumentos da função
+        descriptor.value = function (...args: any[]) {
+
+            if (args[1] === true) {
+                console.log('Usuário já postou')
+                return null
+            } else {
+                //retorna para continuar a execução da função
+                return childFunction.apply(this, args)
+            }
+        }
+
+        return descriptor
+    }
+}
+
+class Post {
+    alreadyPosted = false
+
+    @checkIfUserPost()
+    post(content: string, alreadyPosted: boolean) {
+        this.alreadyPosted = true
+        console.log(`Post do usuário: ${content}`)
+    }
+}
+
+const newPost = new Post()
+
+newPost.post('Primeiro Post', newPost.alreadyPosted)
+newPost.post('Segundo Post', newPost.alreadyPosted)
+
+//------------------------------------------------------------------------------------
+//Property decorator
+function Max(limit: number) {
+    return function (
+        target: Object,
+        propertyKey: string,
+    ) {
+        let value: string
+
+        const getter = function () {
+            return value
+        }
+
+        const setter = function (newVal: string) {
+            if (newVal.length > limit) {
+                console.log(`Valor deve ter no máxmimo ${limit} digitos`)
+            } else {
+                value = newVal
+            }
+        }
+
+        Object.defineProperty(target, propertyKey, {
+            get: getter,
+            set: setter
+        })
+    }
+}
+class Admin {
+    @Max(10)
+    username
+
+    constructor(username: string) {
+        this.username = username
+    }
+}
+
+const Admin1 = new Admin('123456789012')
+const Admin2 = new Admin('netto')
+
+console.log(Admin1)
+console.log(Admin2)

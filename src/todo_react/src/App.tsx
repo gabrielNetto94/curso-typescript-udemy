af5a3cdaac7b6864488cdfe90b2312fedc2c024f
gabrielNetto94 <gabriel@moinhosul.com.br>
2022-08-24 14:29:22 -0300

    Início projeto todo em react

---
+++ src/todo_react/src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <header>
+        Reactjs + TS
+      </header>
+
+      <h1>Conteúdo</h1>
+
+      <footer>
+        <p>
+          <span>
+            Reactjs + TS todo @ {new Date().getFullYear()}
+          </span>
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
